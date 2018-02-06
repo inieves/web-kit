85ac7cf5c191aa299d6843e0edeeaf57830c13ab
Ian Morris Nieves <imnieves@gmail.com>
2018-02-05 18:02:50 -0800

    mostly got everything working

--- code/index.php
+++ code/index.php
@@ -1,2 +1,18 @@
 <?php
+
+error_log ('!!! error logging is working !!!');
+
+$servername = "db";
+$username = "user";
+$password = "aDGLOYvLfDkeojsn1VgiJku";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+	die("DB Connection failed: " . $conn->connect_error);
+}
+echo "DB Connected successfully";
+
 echo phpinfo();
