d9c9db2942579a84a4929fcb754c453f522d4619
Ian Morris Nieves <imnieves@gmail.com>
2018-02-07 14:31:56 -0800

    added db connection test back in

--- code_php/info.php
+++ code_php/info.php
@@ -2,4 +2,24 @@
 
 error_log ('!!! error logging is working !!!');
 
+$host = getenv('MYSQL_HOST');
+$user = getenv('MYSQL_USER');
+$pass = getenv('MYSQL_PASSWORD');
+$db = getenv('MYSQL_DATABASE');
+
+echo 'DB INFO' . '<br>';
+echo 'host: ' . $host . '<br>';
+echo 'user: ' . $user . '<br>';
+echo 'pass: ' . $pass . '<br>';
+echo 'db: ' . $db . '<br>';
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
+
+// Check connection
+if (!$conn) {
+	echo "status: connection failed: " . mysqli_connect_error();
+}
+echo "status: connection success";
+
 echo phpinfo();
