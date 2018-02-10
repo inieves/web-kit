e94548ef4b6a8aaf439ea2f6e8116cc39f2e09b0
Ian Morris Nieves <imnieves@gmail.com>
2018-02-10 13:23:19 -0800

    fixed bugs

--- config/public/scripts/deploy/host-deploy.sh
+++ config/public/scripts/deploy/host-deploy.sh
@@ -25,7 +25,6 @@ sudo chmod +x /usr/local/bin/docker-compose
 # ADD EUGENE
 adduser --disabled-password --gecos "" eugene && \
 echo 'eugene ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers && \
-usermod -aG sudo eugene && \
 mkdir /home/eugene/.ssh && \
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCp2cpxJXirif4JQJWBXg3bWZVjWRZgPNDzzubTxIzaGzUw1KbWG+D2E/oc1D81ZHKe/IhCHhLoXeB5P7hZKuex3xiTTA6nL/uQi0K9eGu0BpBC6kpY17vcTE4W+F8r/51JaWAUeqf+SriepezAvlfwvAUhLhNieRyAdXa9SCsYHT2gcnBiZhEyFJk07iPYxnvNb5ITe6y2q6TpmmXX7kUKi+h4XNO7NWB6UuwmP3VxrkbX+Suwn0KozinYjAW8yhTJX2Xg06ZfFLNB5DKFr9oHY5pj8ZotUPYiVJNWIYVMO0F6+0DSmJtkWEh02/aTStXa8Os1N8G9o+mLhAueZOzz me@book' >> /home/eugene/.ssh/authorized_keys && \
 chown -R eugene:eugene /home/eugene/.ssh && \
@@ -37,6 +36,7 @@ echo 'eval `ssh-agent -s`' >> /home/eugene/.profile
 # ADD IAN
 adduser --disabled-password --gecos "" ian && \
 echo 'ian ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers && \
+usermod -aG sudo ian && \
 mkdir /home/ian/.ssh && \
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+ANwxfP7t6UKITbuEGAWZa6GcMel43Wu8ILZoM7tatO5rlinSsmhzrOJCBwr5fsG9ztE2twf+YyZATf8n5FjE7PJB/TKw1oneQEsywLx2HFzWrSgwQlUKcyofCRicQKbXFIyb7nOBu+OhDZpJEyiJnkwGLvbDxrNAjO9NOR5v9V17ouaqG4kQrzYkOE6ywAHngZDwMrBhKQJ0K5vSDT4giAe5dVOqbiSwtQ2oyW6ZHdLovvwbFqPGTA9gqBJrJuU9JAho1lrUdD7DAoOS7xXgSC2B/V1DLPOtrINDDoax3LHLn9cfMqRnHV9wkWq1dJ01WKO82iCQvoXsP1nIGkIVNQJiJJcHYO+KUV4kWUsngizhuRap4Uih3pX8QR+BJdzrvYQA9HBoMgKU2+/PoisneedVlRqmWAMMzfYm3z7HuPVzBzQHU0Fir3GMN0p+BQE2fY7ZZS9j2vliWw3ZgbHpFCoaPG/HLciHchc+btTIy6s0CNvOL8MPfEHX2p0CDdY4q7i7Z9QR4FcoDFSR6d6L/VkzvckgEWQyrdTSp/DOflXAeRo0LnxaAMOJ/4HcPvq2L0SY9TmLQshzqP2zoCLKl10/63hjN6SyXOFY/oXT30Z4GC8D/OEuO80OCcylFWy71h6MN9STov7qbl+xB57ZG9rkGGQwvinkwxKhBRiuJQ== imnieves@gmail.com' >> /home/ian/.ssh/authorized_keys && \
 chown -R ian:ian /home/ian/.ssh && \
@@ -55,9 +55,11 @@ groupadd --system --gid 1005 mysql && \
 useradd --system --gid 1005 --uid 1005 mysql
 
 # ADD IAN AND EUGENE TO ALL GROUPS
+usermod -a -G sudo eugene && \
 usermod -a -G music eugene && \
 usermod -a -G mysql eugene && \
 usermod -a -G www-data eugene && \
+usermod -a -G sudo ian && \
 usermod -a -G music ian && \
 usermod -a -G mysql ian && \
 usermod -a -G www-data ian
@@ -77,9 +79,10 @@ cd /var/music
 
 # CLONE GIT REPO
 su ian
+
 eval `ssh-agent -s` && \
 ssh-add ~/.ssh/id_rsa.ian_at_github && \
-git clone --shared git@github.com:inieves/music.git /var/music
+git clone --shared git@github.com:inieves/music.git /var/music && \
 exit
 
 ################
@@ -93,27 +96,35 @@ exit
 # FIX PERMISSIONS #
 ###################
 
+# SET OWNER TO ROOT
+chown -R root /var/music
+
+# PREVENT OTHER FROM READING ANY FILES
+#find /var/music -type f | xargs chmod o=
+
 # CODE
-chgrp -R www-data /var/music/app/code
+chgrp -R www-data /var/music/app/code && \
 chmod -R g=rwx    /var/music/app/code
 
 ## DB
-chgrp -R mysql /var/music/app/db
-chmod -R g=rwx /var/music/app/db
+chgrp -R mysql /var/music/app/db && \
+chmod -R g=rwx /var/music/app/db && \
+chgrp mysql    /var/music/docker/db/mysql.cnf && \
 
 ## LOGS
-chgrp -R mysql    /var/music/app/logs/db
-chmod -R g=rwx    /var/music/app/logs/db
-chgrp -R www-data /var/music/app/logs/web
+chgrp -R mysql    /var/music/app/logs/db && \
+chmod -R g=rwx    /var/music/app/logs/db && \
+chgrp -R www-data /var/music/app/logs/web && \
 chmod -R g=rwx    /var/music/app/logs/web
 
 ## STATIC
-chgrp -R www-data /var/music/app/static
-chmod -R g=rw     /var/music/app/static
+chgrp -R www-data /var/music/app/static && \
+chmod -R g=rwx    /var/music/app/static
 
 ## CONFIG PRIVATE
-find /var/music/config/private/prod/auth/db -type f | xargs chgrp mysql
-find /var/music/config/private/prod/ssl/db -type f | xargs chgrp mysql
+find /var/music/config/private/prod/auth/db -type f | xargs chgrp mysql && \
+find /var/music/config/private/prod/ssl/db  -type f | xargs chgrp mysql && \
+find /var/music/config/private/prod/ssl/web -type f | xargs chgrp www-data
 
 #########################################################
 # users must put their own github private keys in place #
